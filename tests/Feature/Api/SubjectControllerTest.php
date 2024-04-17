24293530ba30b9c01f9fb31ac336ad388c5d9a25
kousuke-funabshi <128987004+kousuke-funabshi@users.noreply.github.com>
2024-04-17 15:35:30 +0900

    Subjectモデルをインポートしました。

--- tests/Feature/Api/SubjectControllerTest.php
+++ tests/Feature/Api/SubjectControllerTest.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 //テストを実行しているあいだだけ有効なトランザクション を作ってくれるトレイト
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+// Subjectモデルをインポート
+use App\Models\Subject;
 
 class SubjectControllerTest extends TestCase
 {

c2bf15e47434e4f0e709a04dc214a85daab9f8c5
kousuke-funabshi <128987004+kousuke-funabshi@users.noreply.github.com>
2024-04-18 15:19:50 +0900

    SubjectFactory.php ⚫️ダミーデータを作りました

--- database/factories/SubjectFactory.php
+++ database/factories/SubjectFactory.php
@@ -6,7 +6,30 @@ namespace Database\Factories;
 use App\Models\Subject;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Subject>
+ */
 class SubjectFactory extends Factory
 {
-   
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Subject::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'company' => $this->faker->company(),
+            'address' => $this->faker->address(),
+            'telephone' =>  $this->faker->phoneNumber(),
+            'representative' => $this->faker->name()
+        ];
+    }
 }

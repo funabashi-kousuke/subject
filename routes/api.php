6d092ed497778b5615fb5938f89078beaf0e7329
kousuke-funabshi <128987004+kousuke-funabshi@users.noreply.github.com>
2024-04-18 16:12:17 +0900

    api.php ⚫️updateアクションを追加

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('subject/create', [SubjectController::class, 'store'])->name('api.subject.create');
 Route::get('subject/show/{id}', [SubjectController::class, 'show'])->name('api.subject.show');
-
+Route::put('subject/update/{id}', [SubjectController::class, 'update'])->name('api.subject.update');

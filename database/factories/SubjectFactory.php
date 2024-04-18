8ace278f65c9385aa38063e987c0b1f6614789e5
kousuke-funabshi <128987004+kousuke-funabshi@users.noreply.github.com>
2024-04-18 13:29:40 +0900

    SubjectFactory.php ⚫️Subjectモデルをインポート

--- database/factories/SubjectFactory.php
+++ database/factories/SubjectFactory.php
@@ -2,19 +2,11 @@
 
 namespace Database\Factories;
 
+//Subjectモデルをインポート
+use App\Models\Subject;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class SubjectFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
-    {
-        return [
-            //
-        ];
-    }
+   
 }

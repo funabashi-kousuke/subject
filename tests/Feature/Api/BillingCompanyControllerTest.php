b2f5b4322015b1c92a06fdefe9228de5bf51991b
kousuke-funabshi <128987004+kousuke-funabshi@users.noreply.github.com>
2024-04-23 16:47:43 +0900

    BillingCompanyControllerTest.php ⚫️請求先情の新規登録

--- tests/Feature/Api/BillingCompanyControllerTest.php
+++ tests/Feature/Api/BillingCompanyControllerTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature\Api;
 
 // BillingCompanyモデルをインポート
 use App\Models\BillingCompany;
+// Subjectモデルをインポート
+use App\Models\Subject;
 // factoryが使えるようになる
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 //テストを実行しているあいだだけ有効なトランザクション を作ってくれるトレイト
@@ -14,15 +16,33 @@ use Tests\TestCase;
 
 class BillingCompanyControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use DatabaseTransaction;
+    use Subject;
 
-        $response->assertStatus(200);
+     // この中に各テストが実行されるたびにしたい処理などがあれば書く
+    // :voidはタイプヒンティングと呼ばれるもの
+    public function setUp():void
+    {
+        parent::setUp();
     }
+
+    // storeアクションに関するテスト
+    /**
+     * @test
+     */
+    public function 請求先情報の登録()
+    {
+        $params = [
+            'subjects_id' => '1',
+            'billing_source' => '太郎会社',
+            'billing_companie' => 'jirou会社',
+            'address' => '東京都',
+            'telephone' => '0123456789',
+            'billing_department' => 'A部'
+        ];
+
+        $res = $this->postJson(route('api.billing_company.create'), $params);
+        dd($res);
+    }
+    // storeアクションに関するテスト
 }

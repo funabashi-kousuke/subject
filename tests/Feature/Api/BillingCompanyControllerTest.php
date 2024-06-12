98e7b811ab41f42b2827da07131679cce951b1ad
kousuke-funabshi <128987004+kousuke-funabshi@users.noreply.github.com>
2024-06-12 12:52:04 +0900

    ⚫️請求先情報の新規作成時にbilling_departmentがnullだった場合に新規登録が失敗する

--- tests/Feature/Api/BillingCompanyControllerTest.php
+++ tests/Feature/Api/BillingCompanyControllerTest.php
@@ -128,5 +128,23 @@ class BillingCompanyControllerTest extends TestCase
             $res->assertstatus(422);
         }
 
+        /**
+         * @test
+         */
+        public function 請求先情報の新規作成時にbilling_departmentがnullだった場合に新規登録が失敗する()
+        {
+            $subject = Subject::factory()->create();
+            $params = [
+                'subject_id' => $subject->id,
+                'billing_source' => '請求元会社',
+                'billing_companie' => '請求先会社',
+                'address' => '東京都',
+                'telephone' => '123456789',
+                'billing_department' => null,
+            ];
+            $res = $this->postJson(route('api.billing_company.create'),$params);
+            $res->assertstatus(422);
+        }
+
     // storeアクションに関するテスト
 }
